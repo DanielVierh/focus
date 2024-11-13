93951af4124a60360d90f443b580b63903ceb77b
DanielVierh <daniel.vierheilig89@yahoo.de>
2024-11-13 23:17:25 +0100

    save and autosave

--- src/js/index.js
+++ src/js/index.js
@@ -151,12 +151,19 @@ function run_pause() {
             is_timer_running = false;
             btn_run_pause.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-play-fill" viewBox="0 0 16 16">
                         <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393"/>
-                    </svg>`;
+            </svg>`;
+            save_into_storage();
         }
     }
 }
+let counter = 0;
 setInterval(() => {
     if (is_timer_running === true) {
+        counter++;
+        if (counter === 60) {
+            counter = 0;
+            save_into_storage();
+        }
         const timers = document.querySelectorAll('.focus-timer');
         const index = save_object.active_timer;
         save_object.sub_timers[index].elapsed_time++;
